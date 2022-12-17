704082d0297a74de37348dd87a9eeaa05909e8a8
kaue <kauecosta98@hotmail.com>
2022-12-17 17:40:33 -0300

    fix: Corrige componente

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -1,25 +1,11 @@
 import { Linking,View,Text} from "react-native";
 import {styles} from "../LinkButton/styles"
 
-
-/*
-  const [url, setUrl] = useState('');
-  const [link, setLink] = useState(false) */
-
   type VideoListProps = { 
       link: string     
     }
   
-  const mock = [
-    {
-      link: 'https://www.youtube.com/watch?v=FJ3UIp7GN6k',
-    }
-  ]
-
-  
- 
-
-    const LinkVideo = ({link}: VideoListProps) => {
+    export const LinkVideo = ({link}: VideoListProps) => {
     return (
 
       <View>
@@ -31,6 +17,3 @@ import {styles} from "../LinkButton/styles"
 
     )
    }
-
-   export default LinkVideo
-   
